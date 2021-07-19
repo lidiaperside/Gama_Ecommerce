8c6a9f4fb30710a45c11162d95fde51ee6981431
Lidia Perside <lidia.perside@hotmail.com>
2021-07-18 23:44:32 -0300

    alterações

--- js/script.js
+++ js/script.js
@@ -2,4 +2,8 @@ const form = document.getElementById('form');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    console.log(nome, email);
+    alert("Cadastro feito com sucesso! ;)")
 })
